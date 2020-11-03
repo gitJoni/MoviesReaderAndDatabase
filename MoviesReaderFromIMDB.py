b77787f0adf0ada39337a59d67a1ea9e31b3eaca
gitJoni <joni.rapatti@gmail.com>
2020-11-03 15:04:47 +0200

    Added methods and updated code.

--- MoviesReaderFromIMDB.py
+++ MoviesReaderFromIMDB.py
@@ -3,7 +3,8 @@ from selenium import webdriver
 
 class MoviesReader:
     def __init__(self):
-        pass
+        self.chrome = webdriver.Chrome()
+        self.chrome.get("https://www.imdb.com/chart/top/?ref_=nv_mv_250") # Starting search from Top 250 page
     
     def checking_for_apostrophe(self, line:str) -> bool:
         for letter in line:
@@ -14,16 +15,59 @@ class MoviesReader:
     def return_working_line_with_SQL(self, line:str) -> str:
         return "".join(map(lambda letter : (letter * 2) if letter == "'" else letter, line))
     
+    def start_search_from_page(self, page:str) -> None:
+        self.chrome.get(page)
+        
+    def get_director_element(self) -> str:
+        try:
+            return self.chrome.find_element_by_xpath('//*[@id="title-overview-widget"]/div[2]/div[1]/div[2]/a').text
+        except Exception as error:
+            print(error, "\nRan to an error while fetching director element, trying another xpath.")
+        try:
+            return self.chrome.find_element_by_xpath('//*[@id="title-overview-widget"]/div[2]/div[2]/div[1]/div[2]/a').text
+        except Exception as error:
+            print(error, "\nRan to an another error, could not fetch director element")
+            return error
+    
+    def get_rating_element(self) -> str:
+        try:
+            return float(self.chrome.find_element_by_xpath('//*[@id="title-overview-widget"]/div[1]/div[2]/div/div[1]/div[1]/div[1]/strong/span').text)
+        except Exception as error:
+            return error
+        
+    def get_title_element(self) -> str:
+        try:
+            return self.chrome.find_element_by_xpath('//*[@id="title-overview-widget"]/div[1]/div[2]/div/div[2]/div[2]/h1').text.split(" (")[0]
+        except Exception as error:
+            return error
+    
+    def get_year_element(self) -> int:
+        try:
+            return int(self.chrome.find_element_by_xpath('//*[@id="titleYear"]/a').text)
+        except Exception as error:
+            return error
+    
+    def sleep_awhile_for_pages_to_download(self, how_long:float or int) -> None:
+        time.sleep(how_long)
+    
+    def run_program(self):
+        
+        
 class TestModule(unittest.TestCase):
+    Reader = MoviesReader()
     def test_apostrophe(self):
-        self.assertEqual(MoviesReader().checking_for_apostrophe("Conan O'Brian"), True)
-        self.assertEqual(MoviesReader().checking_for_apostrophe("Whitney Houston"), False)
-        self.assertEqual(MoviesReader().checking_for_apostrophe("'"), True)
+        self.assertEqual(self.Reader.checking_for_apostrophe("Conan O'Brian"), True)
+        self.assertEqual(self.Reader.checking_for_apostrophe("Whitney Houston"), False)
+        self.assertEqual(self.Reader.checking_for_apostrophe("'"), True)
     
     def test_apostrophe_returned_line(self):
-        self.assertEqual(MoviesReader().return_working_line_with_SQL("Conan O'Brian"), "Conan O''Brian")
-        self.assertEqual(MoviesReader().return_working_line_with_SQL("Whitney Houston"), "Whitney Houston")
-        self.assertEqual(MoviesReader().return_working_line_with_SQL("'"), "''")
+        self.assertEqual(self.Reader.return_working_line_with_SQL("Conan O'Brian"), "Conan O''Brian")
+        self.assertEqual(self.Reader.return_working_line_with_SQL("Whitney Houston"), "Whitney Houston")
+        self.assertEqual(self.Reader.return_working_line_with_SQL("'"), "''")
+    
+    def test_timing(self):
+        self.assertEqual(self.Reader.sleep_awhile_for_pages_to_download(2), None)
+        self.assertEqual(self.Reader.sleep_awhile_for_pages_to_download(0.2), None)
 
 if __name__ == "__main__":
     unittest.main()
